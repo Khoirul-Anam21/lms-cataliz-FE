30dbbc2ce1e6d2f2d31da6b4000fae66968fecae
Khoirul-Anam21 <khairulanamstud@gmail.com>
2023-05-24 19:09:38 +0700

    login & signup UI

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,6 +1,22 @@
+
+
 <template>
-    <div class="flex flex-col items-center justify-center w-full h-screen">
-        <h1>Ini Halaman Login</h1>
+    <div class="flex flex-col space-y-10 items-center justify-start w-full h-screen bg-slate-400">
+
+        <h1 class="text-4xl font-bold pt-14 text-white">Ini Halaman Login</h1>
+
+        <!-- Login Card -->
+        <div class="flex flex-col px-6 py-8 bg-white items-center space-y-10">
+            <h2 class="text-2xl font-bold">Login</h2>
+            <div class="flex flex-col space-y-2">
+                <input type="text" placeholder="Email" class="auth-input">
+                <input type="text" placeholder="Password" class="auth-input">
+            </div>
+            <div class="w-full flex flex-col space-y-4 px-8">
+                <input type="submit" class="main-btn-wide rounded-md" value="Log In">
+                <p>Don't have account? <router-link to="/signup" class="text-blue-400 cursor-pointer" >Sign Up</router-link></p>
+            </div>
+        </div>
     </div>
     <RouterView />
 </template>
