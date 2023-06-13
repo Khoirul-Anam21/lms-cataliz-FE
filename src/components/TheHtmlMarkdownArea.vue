e23a4b4a99b13b7a7c82849a727773d6fb3d325b
Khoirul-Anam21 <khairulanamstud@gmail.com>
2023-06-13 17:15:11 +0700

    reading input with markdown format

---
+++ src/components/TheHtmlMarkdownArea.vue
@@ -0,0 +1,100 @@
+<script setup lang="ts">
+import { computed, onMounted, ref, watch } from 'vue';
+import Prism from 'prismjs';
+
+const props = defineProps({
+  content: {
+    type: String,
+    required: true
+  }
+})
+
+
+const htmlContent = computed(() => props.content);
+
+watch(htmlContent, (newV: string) => console.log(newV));
+
+
+</script>
+
+<template>
+  <div v-html="props.content"></div>
+</template>
+
+<style scoped>
+
+:deep(h1) {
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: -1.5rem;
+}
+
+:deep(h2) {
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-bottom: -1.5rem;
+}
+
+:deep(h3) {
+  font-size: 1.25rem;
+  font-weight: bold;
+  margin-bottom: -1.5rem;
+
+}
+
+/* Lists */
+:deep(ul),
+:deep(ol) {
+  margin: 0px;
+  padding: 0px;
+  list-style: none;
+  margin-bottom: -1.  5rem;
+  margin-top: -1rem;
+}
+
+:deep(ul li::before) {
+  content: '\2022';
+  margin-right: 0.5rem;
+}
+
+:deep(ul li) {
+  counter-increment: step-counter;
+  margin-bottom: -1.5rem;
+}
+
+:deep(ol li) {
+  counter-increment: step-counter;
+  margin-bottom: -1.5rem;
+}
+
+:deep(ol li::before) {
+  content: counter(step-counter) '.';
+  margin-right: 0.5rem;
+}
+
+/* Other styles */
+:deep(p) {
+  margin-bottom: -1.8rem;
+}
+
+:deep(strong) {
+  font-weight: bold;
+  margin-bottom: -1.8rem;
+}
+
+:deep(em) {
+  font-style: italic;
+  margin-bottom: -1.8rem;
+}
+
+:deep(pre) {
+  background-color: #2C3333;
+  padding: 16px;
+  margin-bottom: -1.5rem;
+}
+
+:deep(code) {
+  color: cadetblue
+}
+
+</style>
--- src/pages/facilitator/addcoursematerial.vue
+++ src/pages/facilitator/addcoursematerial.vue
@@ -5,59 +5,88 @@ import ContentInput from '../../components/course/ContentInput.vue';
 
 <template>
   <div class="mt-20 md:mt-28 mb-20">
-      <div class="mt-20 md:ml-64">
-        <nav class="inline-flex ml-5" aria-label="Breadcrumb">
-          <ol class="inline-flex items-center space-x-1 md:space-x-3">
-            <li class="inline-flex items-center">
-              <router-link to="/mycourse">
-                <a href="#" class="text-xl inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
-                <svg aria-hidden="true" class="w-5 h-5 mr-2 inline-flex" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
+    <div class="mt-20 md:ml-64">
+      <nav class="inline-flex ml-5" aria-label="Breadcrumb">
+        <ol class="inline-flex items-center space-x-1 md:space-x-3">
+          <li class="inline-flex items-center">
+            <router-link to="/mycourse">
+              <a href="#"
+                class="text-xl inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                <svg aria-hidden="true" class="w-5 h-5 mr-2 inline-flex" fill="currentColor" viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z">
+                  </path>
+                </svg>
                 My Course</a>
+            </router-link>
+          </li>
+          <li>
+            <div class="inline-flex items-center">
+              <router-link to="/addcourse">
+                <svg aria-hidden="true" class="w-6 h-6 text-gray-400 inline-flex" fill="currentColor" viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clip-rule="evenodd"></path>
+                </svg>
+                <a href="#"
+                  class="ml-1 text-xl text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">Add
+                  New Course</a>
               </router-link>
-            </li>
-            <li>
-              <div class="inline-flex items-center">
-                <router-link to="/addcourse">
-                  <svg aria-hidden="true" class="w-6 h-6 text-gray-400 inline-flex" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-                  <a href="#" class="ml-1 text-xl text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">Add New Course</a>
-                </router-link>
-              </div>
-            </li>
-          </ol>
-        </nav>
-    
+            </div>
+          </li>
+        </ol>
+      </nav>
+
 
       <div class="mt-10 ml-5">
         <h1 class="text-5xl font-bold">Add New Course</h1>
       </div>
 
       <div class="w-11/12 mt-10 ml-5">
-        <ol class="flex items-center w-full text-sm font-medium text-center text-gray-500 dark:text-gray-400 sm:text-base">
-          <li class="flex md:w-full items-center text-blue-600 dark:text-blue-500 sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
+        <ol
+          class="flex items-center w-full text-sm font-medium text-center text-gray-500 dark:text-gray-400 sm:text-base">
+          <li
+            class="flex md:w-full items-center text-blue-600 dark:text-blue-500 sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
             <router-link to="/addcoursedesc">
-                <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:font-light after:text-gray-200 dark:after:text-gray-500">
-                  <svg aria-hidden="true" class="w-4 h-4 mr-2 sm:w-5 sm:h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
-                   Course<span class="hidden sm:inline-flex sm:ml-2">Description</span>
-                </span>
-              </router-link>
-          </li>
-          <li class="flex md:w-full items-center text-blue-600 dark:text-blue-500 sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
-            <router-link to="/addcoursematerial">
-              <span class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:font-light after:text-gray-200 dark:after:text-gray-500">
-                  <svg aria-hidden="true" class="w-4 h-4 mr-2 sm:w-5 sm:h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
-                   Material<span class="hidden sm:inline-flex sm:ml-2"></span>
+              <span
+                class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:font-light after:text-gray-200 dark:after:text-gray-500">
+                <svg aria-hidden="true" class="w-4 h-4 mr-2 sm:w-5 sm:h-5" fill="currentColor" viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                    clip-rule="evenodd"></path>
+                </svg>
+                Course<span class="hidden sm:inline-flex sm:ml-2">Description</span>
               </span>
             </router-link>
           </li>
-          <li class="flex items-center">
-              <span class="mr-2">Publish</span>
-              Course
-          </li>
-        </ol>
-      </div>
+        <li
+          class="flex md:w-full items-center text-blue-600 dark:text-blue-500 sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
+          <router-link to="/addcoursematerial">
+            <span
+              class="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:font-light after:text-gray-200 dark:after:text-gray-500">
+              <svg aria-hidden="true" class="w-4 h-4 mr-2 sm:w-5 sm:h-5" fill="currentColor" viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                  clip-rule="evenodd"></path>
+              </svg>
+              Material<span class="hidden sm:inline-flex sm:ml-2"></span>
+            </span>
+          </router-link>
+        </li>
+        <li class="flex items-center">
+          <span class="mr-2">Publish</span>
+          Course
+        </li>
+      </ol>
+    </div>
 
-      <ContentInput/>
+    <div class="mx-4">
+      <ContentInput />
     </div>
   </div>
-  <RouterView />
-</template>
+</div>
+<RouterView /></template>
