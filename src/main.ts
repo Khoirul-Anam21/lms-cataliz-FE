72380429c4fb14316c39963488c6b9480686ba91
Khoirul-Anam21 <khairulanamstud@gmail.com>
2023-07-16 20:57:24 +0700

    fix main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,11 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia';
-import VueVideoPlayer from '@videojs-player/vue'
-import 'video.js/dist/video-js.css'
-
 
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(pinia);
 app.use(router);
-app.use(VueVideoPlayer)
 
 app.mount('#app')
